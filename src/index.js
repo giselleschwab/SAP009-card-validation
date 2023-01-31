23dddb50cafb4586cf9f15dec8b9936dd86d7f71
Giselle Schwab Silva <giselleschwab@gmail.com>
2023-01-31 15:24:32 -0300

    Alterações finais

--- src/index.js
+++ src/index.js
@@ -1,3 +1,44 @@
 import validator from './validator.js';
 
-console.log(validator);
+// manipular o botão validar
+
+//declarar o botão e colocar um evento de click
+const validarCC = document.getElementById("button1")
+validarCC.addEventListener('click', creditCardDatas);
+
+
+//função que vai obter os dados do cartão de crédito com os números mascarados
+function creditCardDatas() {
+  const cardNumber = document.getElementById("inputCardNumber");
+  
+  //console.log(creditCardDatas);
+  if (cardNumber.value === "") {
+    alert("Esse campo não pode ficar vazio");
+    return false;
+  } else if (cardNumber.value.length < 16) {
+    alert("Ops, número de cartão incompleto, preencha novamente!");
+    return false;
+  }
+  
+     
+  const valid = validator.isValid(cardNumber.value);
+  const numMaskify = validator.maskify(cardNumber.value);
+  if(valid === true) {
+    alert(`O número do seu cartão com final ${numMaskify} é válido`);
+  } 
+  else {
+    (valid === false)
+    alert("Número de cartão inválido, por favor tente novamente!");
+  }
+     
+}
+
+    
+
+
+
+ 
+
+
+   
+
--- src/validator.js
+++ src/validator.js
@@ -1,5 +1,43 @@
 const validator = {
-  // ...
-};
+  isValid: function (cardNumber) {
+    // converter o número do cartão em uma array e inverter
+    const cardArray = cardNumber.split("");     //o split divide uma string em um array de strings;
+    const newArray = []; //declarar uma variável vazia que armazenará os dígitos de acordo com as condições que eles atendem
+    let totalSum = 0;
+    for(let i = 0; i < cardArray.length; i++){
+      newArray.push(parseInt(cardArray[i])) // o push adiciona valores a um array
+    }
+    newArray.reverse(); //reverse para inverter o número do cartão de crédito
+    //console.log(newArray);
+
+    for(let i = 0; i < newArray.length; i++){
+      if (i % 2 === 1){
+        newArray[i] = newArray[i]*2;
+        if (newArray[i] >=10){
+          newArray[i] = newArray[i]-9; //quando o valor for maior que 10 ele diminui 9
+        
+        }
+      }
+
+      totalSum += newArray[i];
+      //console.log(totalSum);
+    } 
+
+    if (totalSum % 10 === 0){
+      return true;
+    } 
+    else{
+      return false;
+    }
+  },
+  
+  maskify: (cardNumber) => {
+    return cardNumber.slice(0, -4).replace(/./g, "#") + cardNumber.slice(-4)
+  }
+
+   
+}
+   
+
 
 export default validator;
